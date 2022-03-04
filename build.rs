699dff733b48444036e400c166d3fac490b3427c
qy3u <qy3u@outlook.com>
2022-03-04 11:21:56 +0800

    add per-thread-stream feature

--- build.rs
+++ build.rs
@@ -1,16 +1,38 @@
 use std::env;
 fn main() {
-    println!("cargo:rerun-if-changed=cu/tools.cu");
+    let source_path = "cu/tools.cu";
+
+    println!("cargo:rerun-if-changed={}", source_path);
 
     match env::var("NVCC") {
         Ok(var) => which::which(var),
         Err(_) => which::which("nvcc"),
     }
-    .unwrap();
+    .expect("nvcc not found");
 
-    cc::Build::new()
+    let mut builder = cc::Build::new();
+
+    builder
         .cuda(true)
         .cudart("static")
-        .file("cu/tools.cu")
-        .compile("libcutools.a");
+        .file(&source_path);
+
+    builder
+        .flag("--gpu-architecture=sm_86")
+        .flag("-gencode")
+        .flag("arch=compute_86,code=sm_86")
+        .flag("-gencode")
+        .flag("arch=compute_80,code=sm_80")
+        .flag("-gencode")
+        .flag("arch=compute_75,code=sm_75")
+        .flag("-gencode")
+        .flag("arch=compute_70,code=sm_70")
+        .flag("-gencode")
+        .flag("arch=compute_60,code=sm_60");
+
+    if cfg!(feature = "per-thread-stream") {
+        builder.flag("--default-stream").flag("per-thread");
+    }
+
+    builder.compile("libcutools.a");
 }
