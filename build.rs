cab721e77df2a46a21fb50caf451af99d43b64c7
qy3u <qy3u@outlook.com>
2023-01-04 08:04:40 +0000

    update build flag

--- build.rs
+++ build.rs
@@ -15,17 +15,12 @@ fn main() {
     builder.cuda(true).cudart("static").file(&source_path);
 
     builder
-        .flag("--gpu-architecture=sm_86")
-        .flag("-gencode")
-        .flag("arch=compute_86,code=sm_86")
-        .flag("-gencode")
-        .flag("arch=compute_80,code=sm_80")
-        .flag("-gencode")
-        .flag("arch=compute_75,code=sm_75")
-        .flag("-gencode")
-        .flag("arch=compute_70,code=sm_70")
-        .flag("-gencode")
-        .flag("arch=compute_60,code=sm_60");
+        .flag("-arch=sm_86")
+        .flag("--generate-code=arch=compute_86,code=sm_86")
+        .flag("--generate-code=arch=compute_80,code=sm_80")
+        .flag("--generate-code=arch=compute_75,code=sm_75")
+        .flag("--generate-code=arch=compute_70,code=sm_70")
+        .flag("--generate-code=arch=compute_61,code=sm_61");
 
     if cfg!(feature = "stream-per-thread") {
         builder.flag("--default-stream").flag("per-thread");
